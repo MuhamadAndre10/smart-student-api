755a4cecd9b42e9176cac9fcac4f1457fdcc4f84
MuhamadAndre10 <andrepriyanto95@gmail.com>
2024-05-27 14:23:31 +0700

    add: converter entity to model

--- student-service/internal/models/converter/student_converter.go
+++ student-service/internal/models/converter/student_converter.go
@@ -1,1 +1,24 @@
 package converter
+
+import (
+	"github.com/MuhamadAndre10/student-profile-service/internal/entity"
+	"github.com/MuhamadAndre10/student-profile-service/internal/models"
+)
+
+func StudentToResponse(students *entity.Students) *models.StudentsResponse {
+	return &models.StudentsResponse{
+		ID:         students.ID,
+		StudentsID: students.StudentsID,
+		FullName:   students.FullName,
+		Address:    students.Address,
+		BrithDate:  students.BrithDate,
+		Gender:     students.Gender,
+		Photo:      students.Photo,
+		Religion:   students.Religion,
+		ClassID:    students.ClassID,
+		Email:      students.Email,
+		Phone:      students.Phone,
+		CreatedAt:  students.CreatedAt,
+		UpdatedAt:  students.UpdatedAt,
+	}
+}
--- student-service/internal/models/students_model.go
+++ student-service/internal/models/students_model.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+type StudentsResponse struct {
+	ID         uuid.UUID `json:"id"`
+	StudentsID string    `json:"students_id"`
+	FullName   string    `json:"full_name"`
+	Email      string    `json:"email"`
+	Phone      string    `json:"phone"`
+	Gender     string    `json:"gender"`
+	Religion   string    `json:"religion"`
+	BrithDate  time.Time `json:"brith_date"`
+	Address    string    `json:"address"`
+	Photo      string    `json:"photo"`
+	ClassID    uuid.UUID `json:"-"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
 type InsertStudent struct {
 	StudentsID string    `json:"students_id"`
 	FullName   string    `json:"full_name"`
